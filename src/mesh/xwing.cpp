b8f25b6ecace672a7055ec991e85876d17054fbe
Aiden Fox Ivey <aiden@aidenfoxivey.com>
2025-07-21 20:15:35 -0400

    wip: Create header file

--- src/mesh/xwing.cpp
+++ src/mesh/xwing.cpp
@@ -1,11 +1,8 @@
-// https://www.ietf.org/archive/id/draft-connolly-cfrg-xwing-kem-08.html
-// Aiden Fox Ivey (c) 2025
+/* A C++-11 (Arduino compliant) implementation of XWING. */
+/* https://www.ietf.org/archive/id/draft-connolly-cfrg-xwing-kem-08.html */
+/* Aiden Fox Ivey (c) 2025 */
 
-#include <array>
-#include <cstdint>
-#include <stdint.h>
-#include <string>
-#include <tuple>
+#include <algorithm>
 
 #include <Curve25519.h>
 
@@ -14,99 +11,45 @@
 #include "ml-kem-768/indcpa.h"
 #include "ml-kem-768/randombytes.h"
 
-const char *XWING_LABEL = "\\.//^\\";
-#define XWING_LABEL_BYTES 6
+#include "xwing.h"
 
-//  ┌──────────────────────┐
-//  │                      │
-//  │        KeyGen        │
-//  │                      │
-//  │                      │
-//  └──────────────────────┘
-//              │
-//             ┌┴──────────────────────────────────┐
-//             ▼                                   ▼
-//    ┌─────────────────┐                 ┌─────────────────┐
-//    │Decapsulation Key│                 │Encapsulation Key│
-//    └─────────────────┘                 └─────────────────┘
-//             │                                   │
-//             │                                   └────────┐
-//             ▼                                            ▼
-// ┌──────────────────────┐                     ┌──────────────────────┐
-// │                      │     ┌──────────┐    │                      │
-// │        Decaps        │  ┌──│Ciphertext│◀┐  │        Encaps        │
-// │                      │◀─┘  └──────────┘ └──│                      │
-// │                      │                     │                      │
-// └──────────────────────┘                     └──────────────────────┘
-//             │                                            │
-//             │                                            │
-//             ▼                                            ▼
-// ┌──────────────────────┐                     ┌──────────────────────┐
-// │    Alice's shared    │                     │     Bob's shared     │
-// │      secret key      │                     │      secret key      │
-// │                      │                     │                      │
-// └──────────────────────┘                     └──────────────────────┘
+/**
+ * ┌──────────────────────┐
+ * │                      │
+ * │        KeyGen        │
+ * │                      │
+ * │                      │
+ * └──────────────────────┘
+ *            │
+ *           ┌┴──────────────────────────────────┐
+ *           ▼                                   ▼
+ * ┌─────────────────┐                 ┌─────────────────┐
+ * │Decapsulation Key│                 │Encapsulation Key│
+ * └─────────────────┘                 └─────────────────┘
+ *            │                                   │
+ *            │                                   └────────┐
+ *            ▼                                            ▼
+ * ┌──────────────────────┐                     ┌──────────────────────┐
+ * │                      │     ┌──────────┐    │                      │
+ * │        Decaps        │  ┌──│Ciphertext│◀┐  │        Encaps        │
+ * │                      │◀─┘  └──────────┘ └──│                      │
+ * │                      │                     │                      │
+ * └──────────────────────┘                     └──────────────────────┘
+ *            │                                            │
+ *            │                                            │
+ *            ▼                                            ▼
+ * ┌──────────────────────┐                     ┌──────────────────────┐
+ * │    Alice's shared    │                     │     Bob's shared     │
+ * │      secret key      │                     │      secret key      │
+ * │                      │                     │                      │
+ * └──────────────────────┘                     └──────────────────────┘
+ */
 
-#define X_SK_BYTES 32
-#define X_PK_BYTES 32
-#define X_CT_BYTES 32
-#define X_SS_BYTES 32
-
-#define M_SK_BYTES 2400
-#define M_PK_BYTES 1184
-#define M_CT_BYTES 1088
-#define M_SS_BYTES 32
-
-#define XWING_SK_BYTES 32
-#define XWING_PK_BYTES 1216
-#define XWING_CT_BYTES 1120
-#define XWING_SS_BYTES 32
-
-#define COMBINED_BYTES (M_SS_BYTES + X_SS_BYTES + X_CT_BYTES + X_PK_BYTES + XWING_LABEL_BYTES)
-
-// Sorry guys, I know better types are possible
-struct MSecretKey {
-    std::array<uint8_t, M_SK_BYTES> b;
-};
-
-struct MPublicKey {
-    std::array<uint8_t, M_PK_BYTES> b;
-};
-
-struct MSharedSecret {
-    std::array<uint8_t, M_SS_BYTES> b;
-};
-
-struct XWingSecretKey {
-    std::array<uint8_t, XWING_SK_BYTES> b;
-};
-
-struct XWingPublicKey {
-    std::array<uint8_t, XWING_PK_BYTES> b;
-};
-
-struct XWingSharedSecret {
-    std::array<uint8_t, XWING_SS_BYTES> b;
-};
-
-struct XSecretKey {
-    std::array<uint8_t, X_SK_BYTES> b;
-};
-
-struct XPublicKey {
-    std::array<uint8_t, X_PK_BYTES> b;
-};
-
-struct XSharedSecret {
-    std::array<uint8_t, X_SS_BYTES> b;
-};
-
-struct XCipherText {
-    std::array<uint8_t, X_CT_BYTES> b;
-}
-
-std::tuple<MSecretKey, XSecretKey, MPublicKey, XPublicKey>
-expand_decapsulation_key(const XWingSecretKey &sk)
+/**
+ * Expand an XWING decapsulation key `sk` into its corresponding ML-KEM-768 and
+ * X25519 keypairs.
+ */
+std::tuple<MSecretKey, XSecretKey, MPublicKey, XPublicKey> expand_decapsulation_key(const XWingSecretKey &sk)
 {
     std::array<uint8_t, 96> expanded;
     XSecretKey x_sk;
@@ -115,11 +58,11 @@ expand_decapsulation_key(const XWingSecretKey &sk)
     MPublicKey m_pk;
 
     shake256(expanded.data(), 96, sk.b.data(), XWING_SK_BYTES);
-    // Implicitly reads 64 bytes from expanded.data()
+    /* Implicitly reads 64 bytes from expanded */
     indcpa_keypair_derand(m_pk.b.data(), m_sk.b.data(), expanded.data());
-    // Take from start + 64 bytes of expanded data
-    memcpy(x_sk.b.data(), expanded.data() + 64, 32);
-    // When given nullptr, `eval` uses the X25519 base point on the elliptic curve
+    /* Read 32 bytes from offsets 64 to 96 */
+    std::copy(expanded.begin() + 64, expanded.end(), x_sk.b.begin());
+    /* When given nullptr, `eval` uses the X25519 base point on the elliptic curve */
     Curve25519::eval(x_pk.b.data(), x_sk.b.data(), nullptr);
 
     return {m_sk, x_sk, m_pk, x_pk};
@@ -137,16 +80,57 @@ std::tuple<XWingSecretKey, XWingPublicKey> generate_key_pair(void)
     randombytes(xwing_sk.b.data(), XWING_SK_BYTES);
     std::tie(m_sk, x_sk, m_pk, x_pk) = expand_decapsulation_key(xwing_sk);
 
-    // Eat your lettuce!
-    memcpy(xwing_pk.b.data(), m_pk.b.data(), M_PK_BYTES);
-    memcpy(xwing_pk.b.data() + M_PK_BYTES, x_pk.b.data(), X_PK_BYTES);
+    auto it = std::copy(m_pk.b.begin(), m_pk.b.end(), xwing_pk.b.begin());
+    std::copy(x_pk.b.begin(), x_pk.b.end(), it);
 
     return {xwing_sk, xwing_pk};
 }
 
-// Deterministic version of key generation function.
-// According to spec this is optional but not required
-std::tuple<XWingSecretKey, XWingPublicKey> generate_key_pair_derand(XWingSecretKey xwing_sk)
+XWingSharedSecret combiner(const MSharedSecret &m_ss, const XSharedSecret &x_ss, const XCipherText &x_ct, const XPublicKey &x_pk)
+{
+    XWingSharedSecret xwing_ss;
+    std::array<uint8_t, COMBINED_BYTES> buf;
+
+    auto it = buf.begin();
+
+    it = std::copy(m_ss.b.begin(), m_ss.b.end(), it);
+    it = std::copy(x_ss.b.begin(), x_ss.b.end(), it);
+    it = std::copy(x_ct.b.begin(), x_ct.b.end(), it);
+    it = std::copy(x_pk.b.begin(), x_pk.b.end(), it);
+    std::copy(XWING_LABEL, XWING_LABEL + XWING_LABEL_BYTES, it);
+
+    sha3_256(xwing_ss.b.data(), buf.data(), buf.size());
+
+    return xwing_ss;
+}
+
+std::tuple<XWingSharedSecret, XWingCipherText> encapsulate(const XWingPublicKey &xwing_pk)
+{
+    MPublicKey m_pk;
+    XPublicKey x_pk;
+    XCipherText x_ct;
+    XSharedSecret x_ss;
+    MSharedSecret m_ss;
+    MCipherText m_ct;
+    XWingSharedSecret xwing_ss;
+    XWingCipherText xwing_ct;
+    std::array<uint8_t, 32> ek_x;
+
+    std::copy(xwing_pk.b.begin(), xwing_pk.b.begin() + 1184, m_pk.b.begin());
+    std::copy(xwing_pk.b.begin() + 1184, xwing_pk.b.begin() + 1216, x_pk.b.begin());
+
+    /* Sample from the on-device entropy source */
+    randombytes(ek_x.data(), 32);
+
+    /* As before, setting the second point to nullptr uses X22519_BASE implicity */
+    Curve25519::eval(x_ct.b.data(), x_ek.data(), nullptr);
+    Curve25519::eval(x_ss.b.data(), x_ek.data(), x_pk.b.data());
+
+    std::tie(m_ss)
+}
+
+// START OPTIONAL PIECES
+std::tuple<XWingSecretKey, XWingPublicKey> generate_key_pair_derand(const XWingSecretKey &xwing_sk)
 {
     XWingPublicKey xwing_pk;
     XSecretKey x_sk;
@@ -156,42 +140,10 @@ std::tuple<XWingSecretKey, XWingPublicKey> generate_key_pair_derand(XWingSecretK
 
     std::tie(m_sk, x_sk, m_pk, x_pk) = expand_decapsulation_key(xwing_sk);
 
-    // Eat your lettuce!
-    memcpy(xwing_pk.b.data(), m_pk.b.data(), M_PK_BYTES);
-    memcpy(xwing_pk.b.data() + M_PK_BYTES, x_pk.b.data(), X_PK_BYTES);
+    auto it = std::copy(m_pk.b.begin(), m_pk.b.end(), xwing_pk.b.begin());
+    std::copy(x_pk.b.begin(), x_pk.b.end(), it);
 
     return {xwing_sk, xwing_pk};
 }
 
-XWingSharedSecret combiner(MSharedSecret m_ss, XSharedSecret x_ss, XCipherText x_ct, XPublicKey x_pk)
-{
-    XWingSharedSecret xwing_ss;
-    uint8_t buf[COMBINED_BYTES];
-
-    // Concatenate to the buffer
-    // Yes, I'm sorry it looks yucky
-    memcpy(buf, m_ss.b.data(), M_SS_BYTES);
-    memcpy(buf + M_SS_BYTES, x_ss.b.data(), X_SS_BYTES);
-    memcpy(buf + M_SS_BYTES + X_SS_BYTES, x_ct.b.data(), X_CT_BYTES);
-    memcpy(buf + M_SS_BYTES + X_SS_BYTES + X_CT_BYTES, x_pk.b.data(), X_PK_BYTES);
-    memcpy(buf + M_SS_BYTES + X_SS_BYTES + X_CT_BYTES + X_PK_BYTES, XWING_LABEL, XWING_LABEL_BYTES);
-
-    sha3_256(xwing_ss.b.data(), buf, COMBINED_BYTES);
-
-    return xwing_ss;
-}
-
-std::tuple<XWingSharedSecret, XWingCipherText> encapsulate(XWingPublicKey)
-{
-    MPublicKey m_pk;
-    XPublicKey x_pk;
-    XCipherText x_ct;
-    XSharedSecret x_ss;
-    XWingSharedSecret xwing_ss;
-    XWingCipherText xwing_ct;
-    std::array<uint8_t, 32> ek;
-
-    randombytes(ek.b.data(), 32);
-
-    Curve25519::eval()
-}
+// END OPTIONAL PIECES
---
+++ src/mesh/xwing.h
@@ -0,0 +1,76 @@
+/* A C++-11 (Arduino compliant) implementation of XWING. */
+/* https://www.ietf.org/archive/id/draft-connolly-cfrg-xwing-kem-08.html */
+/* Aiden Fox Ivey (c) 2025 */
+
+#pragma once
+
+#ifndef XWING_H
+#define XWING_H
+
+#include <array>
+#include <cstdint>
+#include <string>
+#include <tuple>
+
+const char *XWING_LABEL = "\\.//^\\";
+constexpr size_t XWING_LABEL_BYTES = 6;
+
+constexpr size_t X_SK_BYTES = 32;
+constexpr size_t X_PK_BYTES = 32;
+constexpr size_t X_CT_BYTES = 32;
+constexpr size_t X_SS_BYTES = 32;
+constexpr size_t M_SK_BYTES = 2400;
+constexpr size_t M_PK_BYTES = 1184;
+constexpr size_t M_CT_BYTES = 1088;
+constexpr size_t M_SS_BYTES = 32;
+constexpr size_t XWING_SK_BYTES = 32;
+constexpr size_t XWING_PK_BYTES = 1216;
+constexpr size_t XWING_CT_BYTES = 1120;
+constexpr size_t XWING_SS_BYTES = 32;
+constexpr size_t COMBINED_BYTES = (M_SS_BYTES + X_SS_BYTES + X_CT_BYTES + X_PK_BYTES + XWING_LABEL_BYTES);
+
+struct MSecretKey {
+    std::array<uint8_t, M_SK_BYTES> b{};
+};
+
+struct MPublicKey {
+    std::array<uint8_t, M_PK_BYTES> b{};
+};
+
+struct MSharedSecret {
+    std::array<uint8_t, M_SS_BYTES> b{};
+};
+
+struct XWingSecretKey {
+    std::array<uint8_t, XWING_SK_BYTES> b{};
+};
+
+struct XWingPublicKey {
+    std::array<uint8_t, XWING_PK_BYTES> b{};
+};
+
+struct XWingSharedSecret {
+    std::array<uint8_t, XWING_SS_BYTES> b{};
+};
+
+struct XWingCipherText {
+    std::array<uint8_t, XWING_CT_BYTES> b{};
+};
+
+struct XSecretKey {
+    std::array<uint8_t, X_SK_BYTES> b{};
+};
+
+struct XPublicKey {
+    std::array<uint8_t, X_PK_BYTES> b{};
+};
+
+struct XSharedSecret {
+    std::array<uint8_t, X_SS_BYTES> b{};
+};
+
+struct XCipherText {
+    std::array<uint8_t, X_CT_BYTES> b{};
+};
+
+#endif
